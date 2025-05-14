70d5f758944595757d6649112e38d1e91c1fc6b2
Aspen <aspenvanderwijst@gmail.com>
2025-05-14 20:50:51 +0200

    Approved grades no longer show up for "need to be approved"

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -11,7 +11,7 @@ class RoleController extends Controller
     public function index()
     {
        if(auth()->user()->role_id == 3){
-           $grades = Grade::with('student', 'assignment')->get();
+           $grades = Grade::where('approved', false)->get();
            return view('admin.dashboard')->with('grades', $grades);
        }
        elseif(auth()->user()->role_id == 2){
