e44f99cd09de853f97bfed6bbe9b0d132d8041e4
Aspen <aspenvanderwijst@gmail.com>
2025-06-09 19:43:21 +0200

    Bug fixes

--- routes/web.php
+++ routes/web.php
@@ -30,24 +30,20 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/student/subjects/{subject}', [StudentController::class, 'subject'])->name('student.subject');
     Route::get('/student/subjects/{subject}/assignments/{assignment}/download', [StudentController::class, 'download'])->name('student.download.grade');
 
-    Route::get('/competentie', function (){
-        return view('grading-form');
-    })->name('grading-form');
-
-    Route::get('/grading-templates', [GradingTemplateList::class, 'index'])->name('grading-template.index');
-    Route::get('/grading-template/{id}', [GradingTemplateList::class, 'show'])->name('grading-template.show');
+    Route::get('/student/form/{id}', [FormStudentViewController::class, 'show'])->name('student-form.show');
 
 });
 
 Route::middleware(['auth', 'verified', 'teacher'])->group(function () {
-
-
     Route::get('/teacher/subjects/{subject}', [TeacherController::class, 'subject'])->name('teacher.subject');
     Route::get('/teacher/subjects/{subject}/assignments/new', [TeacherController::class, 'addAssignment'])->name('new.assignment');
     Route::get('/teacher/subjects/{subject}/assignments/{assignment}', [TeacherController::class, 'assignment'])->name('teacher.assignment');
     Route::get('/teacher/subjects/{subject}/students/{student}', [SubjectController::class, 'student'])->name('add.student.subject');
     Route::get('/assignment/add/{subject}', [AssignmentController::class, 'create'])->name('submit.new.assignment');
     Route::get('/assignment/update/{subject}/{assignment}', [AssignmentController::class, 'update'])->name('update.assignment');
+
+    Route::get('/grading-templates', [GradingTemplateList::class, 'index'])->name('grading-template.index');
+    Route::get('/grading-template/{id}', [GradingTemplateList::class, 'show'])->name('grading-template.show');
 });
 
 Route::middleware(['auth', 'verified', 'admin'])->group(function () {
@@ -60,11 +56,11 @@ Route::middleware(['auth', 'verified', 'admin'])->group(function () {
     Route::get('/user/{user}/update', [UserController::class, 'update'])->name('users.update');
     Route::post('/user/{user}/save', [UserController::class, 'save'])->name('users.save');
     Route::get('/user/{user}/delete', [UserController::class, 'delete'])->name('users.delete');
-});
 
-Route::middleware(['auth', 'verified', 'teacher.or.admin'])->group(function () {
-    Route::post('/grades/{grade}/approve', [ApprovalController::class, 'approve'])->name('grades.approve');
-    Route::post('/grades/{grade}/submit', [ApprovalController::class, 'submit'])->name('grades.submit');
+    Route::get('/admin/grading-results', function () {
+        return view('admin.admin-grading-results-edit');
+    })->name('admin.grading-results');
+
     Route::get('/grading-form/create', function () {
         return view('grading-form');
     })->name('grading-form.create');
@@ -72,15 +68,11 @@ Route::middleware(['auth', 'verified', 'teacher.or.admin'])->group(function () {
     Route::get('/grading-form/edit', function () {
         return view('grading-form-edit');
     })->name('grading-form.edit');
+});
 
-    Route::get('/admin/grading-results', function () {
-        return view('admin.admin-grading-results-edit');
-    })->name('admin.grading-results');
-
-    Route::get('/grading-templates', [GradingTemplateList::class, 'index'])->name('grading-template.index');
-    Route::get('/grading-template/{id}', [GradingTemplateList::class, 'show'])->name('grading-template.show');
-    Route::get('/student/form/{id}', [FormStudentViewController::class, 'show'])->name('student-form.show');
-
+Route::middleware(['auth', 'verified', 'teacher.or.admin'])->group(function () {
+    Route::post('/grades/{grade}/approve', [ApprovalController::class, 'approve'])->name('grades.approve');
+    Route::post('/grades/{grade}/submit', [ApprovalController::class, 'submit'])->name('grades.submit');
 });
 
 require __DIR__.'/auth.php';
